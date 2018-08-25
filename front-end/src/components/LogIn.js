692aa69b36943f46335b898a49d07e37e65f26d8
William Lim <weinianlim26@gmail.com>
2018-08-25 15:28:00 -0700

    remove loading gui from Login.js

--- front-end/src/components/LogIn.js
+++ front-end/src/components/LogIn.js
@@ -18,26 +18,23 @@ class LogIn extends Component {
     const {isMakerLoggedIn, isEaterLoggedIn} = this.props
     return(
       <div>
-        { this.state.isloading 
-          ?  (<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>)
-          :  (<div><Nav isMakerLoggedIn={isMakerLoggedIn} isEaterLoggedIn={isEaterLoggedIn}/>
-            <div className="login-body">
-              <div className="row">
-                <div className="makerlogindiv col-12 col-sm-12 col-md-6 col-lg-6 text-center">
-                  <h1>Maker</h1>
-                  <Link class="nav-link" to={`/makerLogIn`}>
-                    <button type="button" className="btn btn-outline-light btn-lg my-auto mx-auto font-weight-bold">Maker</button>
-                  </Link>
-                </div>
-                <div className="eaterlogindiv col-12 col-sm-12 col-md-6 col-lg-6 text-center">
-                  <h1>Eater</h1>
-                  <Link class="nav-link" to={`/eaterLogIn`}>
-                    <button type="button" className="btn btn-outline-dark btn-lg my-auto mx-auto font-weight-bold">Eater</button>
-                  </Link>
-                </div>
+        <Nav isMakerLoggedIn={isMakerLoggedIn} isEaterLoggedIn={isEaterLoggedIn}/>
+          <div className="login-body">
+            <div className="row">
+              <div className="makerlogindiv col-12 col-sm-12 col-md-6 col-lg-6 text-center">
+                <h1>Maker</h1>
+                <Link class="nav-link" to={`/makerLogIn`}>
+                  <button type="button" className="btn btn-outline-light btn-lg my-auto mx-auto font-weight-bold">Maker</button>
+                </Link>
               </div>
-            </div></div>)
-        }
+              <div className="eaterlogindiv col-12 col-sm-12 col-md-6 col-lg-6 text-center">
+                <h1>Eater</h1>
+                <Link class="nav-link" to={`/eaterLogIn`}>
+                  <button type="button" className="btn btn-outline-dark btn-lg my-auto mx-auto font-weight-bold">Eater</button>
+                </Link>
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
